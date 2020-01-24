918fc8739a92a0708b1040be86e38d16e2af3cd4
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-01-23 18:56:55 -0600

    Person constructor

--- index.js
+++ index.js
@@ -39,9 +39,21 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length > 9) {
+    return `${this.name} is full, nothing can be eaten`;
+  } else {
+    this.stomach.push(someFood);
+  }
+}
+
+
 
 /*
   TASK 2
