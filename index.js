9ef12cfcfb76753383300dddb9ab4367dd84dcd1
ThomasBettencourt <thomasbetten.cs@gmail.com>
2020-01-23 19:16:08 -0600

    finished car finished person

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ Person.prototype.eat = function(someFood) {
   }
 }
 
+Person.prototype.poop = function() {
+  this.stomach.length = 0;
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
+
 
 
 /*
@@ -69,9 +77,16 @@ Person.prototype.eat = function(someFood) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+};
 
-}
+Car.prototype.fill = function(gallons) {
+   this.tank = this.tank + gallons;
+};
 
 /*
   TASK 3
@@ -80,7 +95,7 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
 
 }
 
